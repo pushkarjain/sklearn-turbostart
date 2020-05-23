2db088ba1fa4d07b59101210e6ff20b64f243da1
Pushkar Kumar Jain <pushkar.jain@arundo.com>
2020-05-22 21:49:17 -0500

    Adding type hints and comments

--- model_turbostart/estimators.py
+++ model_turbostart/estimators.py
@@ -1,29 +1,43 @@
+from typing import Dict
 
 from sklearn.compose import TransformedTargetRegressor
-from sklearn.ensemble import (HistGradientBoostingRegressor,
-                              RandomForestRegressor)
+from sklearn.compose._column_transformer import ColumnTransformer
+from sklearn.ensemble import HistGradientBoostingRegressor, RandomForestRegressor
 from sklearn.linear_model import ElasticNet
 from sklearn.model_selection import GridSearchCV
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 
 
-def create_estimators(preprocessing):
+def create_estimators(preprocessing: ColumnTransformer) -> Dict[str, GridSearchCV]:
+    """Create estimators for model fitting.
+
+    ElasticNet, HistGradientBoost and RandomForest regressors are added. 
+    More can be added.
+
+    Args:
+        preprocessing: Preprocessing Pipeline
+
+    Returns:
+        Key-value pairs of estimator name and instantiated estimator
+    """
     # ElasticNet (alpha = 1.0 -> Lasso)
     param_grid = {
         "regressor__regressor__alpha": (0.001, 0.01, 0.1, 1.0),
         "regressor__regressor__l1_ratio": (0.05, 0.2, 0.5, 0.7, 0.9, 1.0),
     }
     en_pipe = TransformedTargetRegressor(
-        regressor=Pipeline([("preprocessor", preprocessing), ("regressor", ElasticNet())]),
+        regressor=Pipeline(
+            [("preprocessor", preprocessing), ("regressor", ElasticNet())]
+        ),
         transformer=StandardScaler(),
     )
     en_search = GridSearchCV(en_pipe, param_grid=param_grid, cv=5,)
 
     # RandomForest
     param_grid = {
-        "regressor__regressor__n_estimators": [50, 100, 200, 300],
-        "regressor__regressor__max_depth": [5, 6, 7, 8, 15, 20],
+        "regressor__regressor__n_estimators": [50, 100, 200],
+        "regressor__regressor__max_depth": [5, 6, 7, 15],
     }
     rf_pipe = TransformedTargetRegressor(
         regressor=Pipeline(
@@ -36,8 +50,8 @@ def create_estimators(preprocessing):
     # HistGradientBoost
     param_grid = {
         "regressor__regressor__l2_regularization": [0.0, 0.1, 1.0],
-        "regressor__regressor__max_depth": [6, 15, 20, None],
-        "regressor__regressor__max_iter": [100, 200, 300],
+        "regressor__regressor__max_depth": [6, 15],
+        "regressor__regressor__max_iter": [100, 200],
     }
     hgb_pipe = TransformedTargetRegressor(
         regressor=Pipeline(
--- model_turbostart/train.py
+++ model_turbostart/train.py
@@ -1,11 +1,14 @@
 import logging
-from typing import Dict, Tuple
+from typing import Dict, List, Tuple
 
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from sklearn.compose._column_transformer import ColumnTransformer
+from sklearn.inspection import permutation_importance
 from sklearn.metrics import mean_squared_error, median_absolute_error
 from sklearn.model_selection import GridSearchCV, train_test_split
+from sklearn.pipeline import Pipeline
 
 plt.style.use("seaborn-talk")
 
@@ -16,6 +19,16 @@ log = logging.getLogger(__name__)
 def load_data(
     data_path: str,
 ) -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+    """Loads data from datapath. Performs train-test split and transforms response to log
+
+    TODO: Figure out doing log transformation in Pipeline itself
+
+    Args:
+        data_path: Path to dataset
+
+    Returns:
+        Train and test data
+    """
     df = pd.read_csv(data_path, sep=";")
     assert len(df) > 0
     df["Y"] = df["Y"].apply(lambda x: np.log(x))
@@ -36,7 +49,16 @@ def train_estimators(
     X_test: pd.DataFrame,
     y_test: pd.DataFrame,
 ) -> None:
-    fig, axes = plt.subplots(1, 3, figsize=(20, 5), sharey=True)
+    """Fit estimators and yield metrics in prediction plots
+
+    Args:
+        estimators: Key-value pairs of estimator names and estimators to be fit
+        X_train: Train features
+        y_train: Train response
+        X_test: Test features
+        y_test: Test response
+    """
+    fig, axes = plt.subplots(1, len(estimators.keys()), figsize=(20, 5), sharey=True)
     for ax, (estimator_name, estimator) in zip(axes, estimators.items()):
         reg = estimator.fit(X_train, y_train)
         y_pred = reg.predict(X_test)
@@ -46,7 +68,11 @@ def train_estimators(
         print(f"Cross-val score: {reg.best_score_:.3f}")
         print(f"Test score: {score:.3f}")
         print(f"----------")
-        scores = f"$R^2$={score:.3f}, MAE={median_absolute_error(y_test, y_pred):.3f}, MSE={mean_squared_error(y_test, y_pred):.3f}"
+        scores = (
+            f"$R^2$={score:.3f}, "
+            f"MAE={median_absolute_error(y_test, y_pred):.3f}, "
+            f"MSE={mean_squared_error(y_test, y_pred):.3f}"
+        )
         plot_prediction(
             y_test=y_test, y_pred=y_pred, ax=ax, scores=scores, name=estimator_name
         )
@@ -55,10 +81,19 @@ def train_estimators(
 def plot_prediction(
     y_pred: np.ndarray,
     y_test: np.ndarray,
-    ax=None,
+    ax: plt.Axes = None,
     scores: str = None,
     name: str = None,
 ) -> None:
+    """Plot prediction vs truth
+
+    Args:
+        y_pred: Predicted values
+        y_test: Ground truth
+        ax: Axes
+        scores: String with scores to show as plot legend
+        name: Title of plot
+    """
     if ax is None:
         f, ax = plt.subplots(1, 1, figsize=(10, 10))
     ax.scatter(y_test, y_pred, alpha=0.3)
@@ -72,3 +107,67 @@ def plot_prediction(
         ax.set_title(name)
     if scores:
         ax.legend([extra], [scores], loc="upper left")
+
+
+def get_column_names_from_ColumnTransformer(
+    column_transformer: ColumnTransformer,
+) -> List[str]:
+    """Get column names from column transformer.
+    
+    This is required because feature importance is poorly compatible with `sklearn` `Pipeline`. 
+
+    Args:
+        column_transformer: Feature transformer
+
+    Returns:
+        Column names after transformation
+    """
+    col_name: List[str] = []
+    # the last transformer is ColumnTransformer's 'remainder'
+    for transformer_in_columns in column_transformer.transformers_:
+        raw_col_name = transformer_in_columns[2]
+        if isinstance(transformer_in_columns[1], Pipeline):
+            transformer = transformer_in_columns[1].steps[-1][1]
+        else:
+            transformer = transformer_in_columns[1]
+        try:
+            names = transformer.get_feature_names()
+        except AttributeError:  # if no 'get_feature_names' function, use raw column name
+            names = raw_col_name
+        if isinstance(names, np.ndarray):
+            col_name += names.tolist()
+        elif isinstance(names, list):
+            col_name += names
+        elif isinstance(names, str):
+            col_name.append(names)
+    return col_name
+
+
+def plot_permutation_importance(
+    estimators: Dict[str, GridSearchCV], X_test: pd.DataFrame, y_test: pd.DataFrame
+) -> None:
+    """Plot permutation importance for estimators
+
+    Args:
+        estimators: Key-value pairs of estimator names and **fitted** estimators
+        X_test: Test features
+        y_test : Test response
+    """
+    fig, axes = plt.subplots(1, len(estimators), figsize=(15, 5))
+    for ax, (estimator_name, estimator) in zip(axes, estimators.items()):
+        est = estimator.best_estimator_
+        result = permutation_importance(
+            est, X_test, y_test, n_repeats=10, random_state=2020
+        )
+        perm_sorted_idx = result.importances_mean.argsort()
+        columns = get_column_names_from_ColumnTransformer(
+            est.regressor_.named_steps["preprocessor"]
+        )
+        log.info(columns)
+        labels = [columns[i] for i in perm_sorted_idx]
+        if ax is not None:
+            fig, ax = plt.subplots(1, 1, figsize=(8, 5))
+        ax.boxplot(
+            result.importances[perm_sorted_idx].T, vert=False, labels=labels,
+        )
+    fig.tight_layout()
